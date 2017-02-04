4acfd8d13a059f738543b5c0aba30ea3d4e274c1
Yutaka Konishi <slash170@gmail.com>
2017-02-04 07:40:27 +0000

    js/css/html: implement any counter app usin react

--- src/main.js
+++ src/main.js
@@ -1,2 +1,37 @@
 /* @flow */
-console.log("hello1a");
+var Counter = React.createClass({
+    getInitialState: function() {
+        return {count: 0};
+    },
+
+    handlePlus: function () {
+        this.setState({
+            count: this.state.count + 1
+        });
+    },
+
+    handleMinus: function () {
+        this.setState({
+            count: this.state.count - 1
+        });
+    },
+
+    render: function() {
+        return (
+            <div>
+                <h3>{this.props.name}の個数</h3>
+                <button onClick={this.handlePlus}>プラス!</button>
+                <div>{this.state.count}個</div>
+                <button onClick={this.handleMinus}>マイナス</button>
+            </div>
+        );
+    }
+});
+var counters = (
+    <div>
+    <Counter name="りんご" />
+    </div>
+);
+
+var content = document.getElementById('content');
+ReactDOM.render(counters, content);
